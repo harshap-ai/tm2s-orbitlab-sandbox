4ab0c8f17917b33f5d99221b5e1e217d0bca85e4
Harish Prakash <srinivas.harish27@gmail.com>
2025-12-24 14:58:49 +0530

    removed patchify, using numpy for patches

--- src/main.py
+++ src/main.py
@@ -1,77 +1,79 @@
-from tensorflow.keras.models import load_model
-from numpy.random import randint
-import numpy as np
-from PIL import Image
+# Written by Harish Prakash for Tiger Analytics on Dec 24, 2025. 
+
 import os
-import rasterio 
-import patchify 
-import math 
+import math
+import numpy as np
+import rasterio
+from PIL import Image
+from tensorflow.keras.models import load_model
 
 class ImageResEnhancer:
-  def __init__(self, modelPath):
-    self.modelPath = modelPath
+    def __init__(self, modelPath):
+        self.modelPath = modelPath
+        self.generator = load_model(self.modelPath, compile=False)
 
-  def loadfile(self,filepath):
-    img = np.load(filepath)
-    if img.shape[0] > 1:
-      img = img[:1]
-    return img
+    def patchify_input(self, image_array):
+        # image_array: (Bands, H, W) -> (H, W, Bands)
+        img = np.moveaxis(image_array, 0, -1)
+        h, w, c = img.shape
+        patch_size = 128 # as taken by our custom model. 
 
-  def modelinfer(self,image_array):
-    generator = load_model(self.modelPath, compile=False)
-    src_image = image_array
-    genimg = generator.predict(src_image)
-    return src_image,genimg
-  
-  def patchify_input(self, image_array):
+        assert h % patch_size == 0 and w % patch_size == 0, "Image dimensions must be multiples of 128"
+        patches = img.reshape(h // patch_size, patch_size, w // patch_size, patch_size, c)
+        patches = patches.transpose(0, 2, 1, 3, 4) 
+        return patches 
 
-    numpy_image = np.moveaxis(image_array, 0, -1)
-    assert numpy_image.shape[0] == numpy_image.shape[1], "Please match height and width dimensions"
-    patches = patchify.patchify(numpy_image, (128, 128, numpy_image.shape[2]), step=128)
-    return patches 
-  
-  def unpatchify_result(self, image_stack):
+    def unpatchify_result(self, image_stack):
+        # image_stack: (Total_Patches, 1, 256, 256, 3) 
+        stack = image_stack.squeeze(axis=1) # Remove the extra dimension
+        num_patches = stack.shape[0]
+        grid_size = int(math.sqrt(num_patches))
+        patch_h, patch_w, c = stack.shape[1:]
+        
+        reshaped = stack.reshape(grid_size, grid_size, patch_h, patch_w, c)
+        transposed = reshaped.transpose(0, 2, 1, 3, 4)
+        unpatched = transposed.reshape(grid_size * patch_h, grid_size * patch_w, c)
+        return unpatched
 
-    divisor = int(math.sqrt(image_stack.shape[0]))
-    image_stack_squeezed = image_stack.squeeze(axis=1) 
-    reshaped_image_stack = image_stack_squeezed.reshape(divisor, divisor, 1, image_stack_squeezed.shape[1], image_stack_squeezed.shape[2], image_stack_squeezed.shape[3])
-    #reshape_image_stack.shape = (divisor, divisor, 1, 256, 256, 3)
+    def modelinfer(self, image_array):
+        # image_array shape expected: (1, 128, 128, 3)
+        genimg = self.generator.predict(image_array, verbose=0)
+        return genimg
 
-    target_h = reshaped_image_stack.shape[0] * reshaped_image_stack.shape[3]
-    target_w = reshaped_image_stack.shape[1] * reshaped_image_stack.shape[4]
-    target_c = reshaped_image_stack.shape[5]
-    unpatched_image = patchify.unpatchify(reshaped_image_stack, (target_h, target_w, target_c))
-    return unpatched_image
-    
+    def imgsave(self, outputpath, genimg, _input_name):
+        clean_name = os.path.splitext(_input_name)[0]
+        save_path = os.path.join(outputpath, f"{clean_name}.png")
+        
+        gen_imageFinal = Image.fromarray(genimg.astype(np.uint8))
+        gen_imageFinal.save(save_path)
+
 if __name__ == "__main__": 
-    
-    modelpath = f"/workspace/models/gen_e_50.h5"
-    inputpath  = f"/opt/ilc_player/data"
-    outputpath = f"/opt/ilc_player/results"
+    modelpath = "/models/gen_e_50.h5"  
+    inputpath  = "/opt/ilc_player/data"
+    outputpath = "/opt/ilc_player/results"
 
     imgEnH = ImageResEnhancer(modelpath)
 
-    for _input_name in os.listdir(inputpath):
-      if _input_name.endswith(".tiff", ".tif"):
-          input_image = os.path.join(inputpath, _input_name)
-          with rasterio.open(input_image) as input:
-            # Assuming array with dimension order (Bands, Height, Width)
-            image_array = input.read()
-            patches = imgEnH.patchify_input(image_array)
-            
-            flat_patches = patches.reshape(-1, 128, 128, 4)
-            _to_unpatch = list()
-            for _input in flat_patches:
-              # Assuming that we take the first 3 channels for our model.  
-              _, genimg = imgEnH.modelinfer(np.expand_dims(_input[:, :, :3], axis=0))
-              _to_unpatch.append(genimg)
-            image_stack = np.stack(_to_unpatch, axis=0)
-            unpatched_image = imgEnH.unpatchify_result(image_stack)
+    for root, dirs, files in os.walk(inputpath):
+        for _input_name in files:
+            if _input_name.lower().endswith((".tiff", ".tif")):
+                input_image_path = os.path.join(root, _input_name)
 
-            unpatched_image.save(os.path.join(outputpath, f"{os.path.splittext(_input_name)[0]}.png"))
-           
-      else: 
-         raise ValueError("Incorrect image format")
-      
-      src_image, genimg = imgEnH.modelinfer(  )
-      imgEnH.imgsave(outputpath,genimg)
+                with rasterio.open(input_image_path) as src:
+                    image_array = src.read() 
+                    patches = imgEnH.patchify_input(image_array)
+                    _, _, p_h, p_w, p_c = patches.shape
+                    
+                    _to_unpatch = []
+                    flat_patches = patches.reshape(-1, p_h, p_w, p_c)
+                    
+                    for patch in flat_patches:
+                        # Taking first 3 channels and adding batch dimension
+                        patch_input = np.expand_dims(patch[:, :, :3], axis=0)
+                        genimg = imgEnH.modelinfer(patch_input)
+                        _to_unpatch.append(genimg)
+                    
+                    # Reconstruct
+                    image_stack = np.stack(_to_unpatch, axis=0)
+                    unpatched_image = imgEnH.unpatchify_result(image_stack)
+                    imgEnH.imgsave(outputpath, unpatched_image, _input_name)
